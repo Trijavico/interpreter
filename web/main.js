710808547ebaeaf2fcac0629db43460fe5d97a04
Trijavico <javier.penaperez08@gmail.com>
2024-10-30 19:22:03 -0400

    improve the ui of the output

--- web/main.js
+++ web/main.js
@@ -6,9 +6,15 @@ let $run = document.querySelector("#run");
 let $console = document.querySelector("#console_output");
 
 $run.addEventListener('click', function() {
+	$run.disabled = true;
+
 	$console.replaceChildren();
 	let output = eval_code(view.state.doc.toString(), $console);
 	if (output != "") {
 		$console.innerHTML += `<span>${output}</span>`;
 	}
+
+	setTimeout(() => {
+		$run.disabled = false;
+	}, 1000);
 });
